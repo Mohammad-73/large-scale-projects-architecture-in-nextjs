495e833dab0a2295939c4974b63d0d7a8b2731f1
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-06 10:42:38 +0330

    Fetch course details with static rendering technique and using generateStaticParams

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,7 +1,32 @@
-export default async function CourseDetailsPage() {
+import { API_URL } from "@/configs/global";
+import { CourseDetails } from "@/types/course-details.interface";
+
+export async function generateStaticParams() {
+  const slugs = await fetch(`${API_URL}/courses/slug`).then((res) =>
+    res.json()
+  );
+
+  return (slugs as string[]).map((slug) => ({
+    slug: slug,
+  }));
+}
+
+async function getCourse(slug: string): Promise<CourseDetails> {
+  const res = await fetch(`${API_URL}/courses/${slug}`);
+  return res.json();
+}
+
+export default async function CourseDetailsPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+  const courseData = await getCourse(slug);
+
   return (
     <div className="text-5xl flex justify-center items-center">
-      <h1>This is Course Details page</h1>
+      <h1>{courseData.title}</h1>
     </div>
   );
 }
