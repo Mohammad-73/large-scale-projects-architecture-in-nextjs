ec1c41deda20ebddb50d1df6683005f482e8632c
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-09 14:07:03 +0330

    Fetch course comments using React query and custom hook

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { createData } from "@/core/http-service/http-service";
+import { useParams } from "next/navigation";
 import { useEffect } from "react";
+import { useCourseComments } from "../../_api/get-comments";
 
 const CourseComments = () => {
-  useEffect(() => {
-    createData("/validation-error", {});
-  }, []);
-  return <></>;
+  const { slug } = useParams();
+  const { data: comments } = useCourseComments({
+    params: {
+      slug: slug as string,
+      page: 1,
+    },
+  });
+  return (
+    <>
+      {comments?.data.map((p) => (
+        <p key={p.id} className="mb-8">
+          {p.commentText}
+        </p>
+      ))}
+    </>
+  );
 };
 
 export default CourseComments;
