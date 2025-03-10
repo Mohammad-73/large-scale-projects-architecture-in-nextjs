eab3090e5a13fd440425d2c7966df08473fe30c7
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-10 12:52:49 +0330

    Implementing Alert component

---
+++ src/app/_components/alert/alert.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from "classnames";
+import { AlertProps } from "./alert.types";
+import { IconInfo } from "../icons/icons";
+
+export const Alert: React.FC<AlertProps> = ({
+  variant,
+  className,
+  showIcon = true,
+  children,
+}) => {
+  const classes = classNames("alert", className, {
+    [`alert-${variant}`]: variant,
+  });
+
+  return (
+    <div className={classes}>
+      {showIcon && <IconInfo width={18} />}
+      {children}
+    </div>
+  );
+};
