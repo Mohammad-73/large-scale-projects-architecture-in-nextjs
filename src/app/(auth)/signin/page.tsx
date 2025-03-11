afdfbf6e114bfde9c13934b8edfa800e01f638f7
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-11 11:56:29 +0330

    Using template instead of layout for auth pages

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default async function SignInPage() {
   return (
-    <div className="text-5xl flex justify-center items-center">
-      <h1>This is SignIn page</h1>
+    <div className="">
+      <Link href="/verify">Go To Verification Page</Link>
     </div>
   );
 }
