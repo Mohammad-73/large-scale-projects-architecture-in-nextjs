d58541c6e4d9faf93ae519edd01bc4b10de658cb
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-23 14:28:21 +0330

    Implementation signout logic

--- src/app/_components/header/header-user-section.tsx
+++ src/app/_components/header/header-user-section.tsx
@@ -1,19 +1,50 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { logout } from "@/actions/auth";
+import { getSession, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useActionState, useEffect } from "react";
+import { useFormStatus } from "react-dom";
+import { Loading } from "../loading";
+import { useRouter } from "next/navigation";
 
 const HeaderUserSection = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const [signOutState, action] = useActionState(logout, undefined);
+
+  useEffect(() => {
+    if (signOutState?.isSuccess) {
+      const fetchSession = async () => await getSession();
+      fetchSession();
+      router.push("/");
+    }
+  }, [signOutState, router]);
 
   return (
     <>
       {session?.user ? (
-        <p>{session.user.mobile}</p>
+        <>
+          <p>{session.user.mobile}</p>
+          <form action={action as () => void}>
+            <LogoutButton />
+          </form>
+        </>
       ) : (
         <Link href="/signin">ورود یا ثبت نام</Link>
       )}
     </>
+  );
+};
+
+const LogoutButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="text-error">
+      {pending && <Loading size="tiny" />}
+      خروج از حساب کاربری
+    </button>
   );
 };
 
