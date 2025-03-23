5a59201bd18c3dc00d9c2746c58a61d5cdbd5550
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-23 13:58:55 +0330

    Implementation auth middleware for protected routes

--- src/app/(acount)/student/layout.tsx
+++ src/app/(acount)/student/layout.tsx
@@ -1,5 +1,3 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 export default async function StudentLayout({
@@ -7,11 +5,6 @@ export default async function StudentLayout({
 }: {
   children: ReactNode;
 }) {
-  const session = await auth();
-  if (!session || !session.user) {
-    redirect("/signin");
-  }
-
   return (
     <>
       <aside className="bg-gray-300 w-80 flex justify-center items-center self-stretch text-3xl">
